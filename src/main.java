5f305c48ebefac070281edf1cdc0e81e4592096b
lgezzby <634065623@qq.com>
2017-08-01 21:47:01 +0800

    * 加锁 + 动态曲线图 * 收敛过程存在问题

--- src/main.java
+++ src/main.java
@@ -1,8 +1,14 @@
 import com.zjgsu.util.DistributedSpectrumAccess;
+import com.zjgsu.util.SocialGroupUtility;
+
 
 public class main {
     public static void main(String[] args){
         DistributedSpectrumAccess algorithm = new DistributedSpectrumAccess();
         algorithm.algorithm();
+
+        /*SocialGroupUtility socialGroupUtility = new SocialGroupUtility(0);
+        socialGroupUtility.plot();*/
+
     }
 }
